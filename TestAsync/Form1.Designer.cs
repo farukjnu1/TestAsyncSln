ed1dedec2ea49e29047e4603cb1534cb02048361
farukjnu1@gmail.com <farukjnu1@gmail.com>
2025-09-18 00:35:53 +0800

    Add project files.

---
+++ TestAsync/Form1.Designer.cs
@@ -0,0 +1,86 @@
+﻿namespace TestAsync
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.StartButton = new System.Windows.Forms.Button();
+            this.ResetButton = new System.Windows.Forms.Button();
+            this.resultsTextBox = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // StartButton
+            // 
+            this.StartButton.Location = new System.Drawing.Point(41, 28);
+            this.StartButton.Name = "StartButton";
+            this.StartButton.Size = new System.Drawing.Size(83, 33);
+            this.StartButton.TabIndex = 0;
+            this.StartButton.Text = "Start Button";
+            this.StartButton.UseVisualStyleBackColor = true;
+            this.StartButton.Click += new System.EventHandler(this.StartButton_Click);
+            // 
+            // ResetButton
+            // 
+            this.ResetButton.Location = new System.Drawing.Point(361, 28);
+            this.ResetButton.Name = "ResetButton";
+            this.ResetButton.Size = new System.Drawing.Size(83, 33);
+            this.ResetButton.TabIndex = 1;
+            this.ResetButton.Text = "Reset";
+            this.ResetButton.UseVisualStyleBackColor = true;
+            this.ResetButton.Click += new System.EventHandler(this.ResetButton_Click);
+            // 
+            // resultsTextBox
+            // 
+            this.resultsTextBox.Location = new System.Drawing.Point(41, 85);
+            this.resultsTextBox.Multiline = true;
+            this.resultsTextBox.Name = "resultsTextBox";
+            this.resultsTextBox.Size = new System.Drawing.Size(403, 200);
+            this.resultsTextBox.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(480, 316);
+            this.Controls.Add(this.resultsTextBox);
+            this.Controls.Add(this.ResetButton);
+            this.Controls.Add(this.StartButton);
+            this.Name = "Form1";
+            this.Text = "Async Example  ";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button StartButton;
+        private System.Windows.Forms.Button ResetButton;
+        private System.Windows.Forms.TextBox resultsTextBox;
+    }
+}
+
---
+++ TestAsync/Form1.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TestAsync
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        // Mark the event handler with async so you can use await in it.  
+        private async void StartButton_Click(object sender, EventArgs e)
+        {
+            // Call and await separately.  
+            //Task<int> getLengthTask = AccessTheWebAsync();  
+            //// You can do independent work here.  
+            //int contentLength = await getLengthTask;  
+
+            int contentLength = await AccessTheWebAsync();
+
+            resultsTextBox.Text +=
+                $"Length of the downloaded string: {contentLength}.\r\n\r\n";
+        }
+
+        // Three things to note in the signature:  
+        //  - The method has an async modifier.   
+        //  - The return type is Task or Task<T>. (See "Return Types" section.)  
+        //    Here, it is Task<int> because the return statement returns an integer.  
+        //  - The method name ends in "Async."  
+        async Task<int> AccessTheWebAsync()
+        {
+            // You need to add a reference to System.Net.Http to declare client.  
+            var client = new System.Net.Http.HttpClient();
+
+            // GetStringAsync returns a Task<string>. That means that when you await the  
+            // task you'll get a string (urlContents).  
+            Task<string> getStringTask = client.GetStringAsync("https://msdn.microsoft.com");
+
+            // You can do work here that doesn't rely on the string from GetStringAsync.  
+            DoIndependentWork();
+
+            // The await operator suspends AccessTheWebAsync.  
+            //  - AccessTheWebAsync can't continue until getStringTask is complete.  
+            //  - Meanwhile, control returns to the caller of AccessTheWebAsync.  
+            //  - Control resumes here when getStringTask is complete.   
+            //  - The await operator then retrieves the string result from getStringTask.  
+            string urlContents = await getStringTask;
+
+            // The return statement specifies an integer result.  
+            // Any methods that are awaiting AccessTheWebAsync retrieve the length value.  
+            return urlContents.Length;
+        }
+
+        void DoIndependentWork()
+        {
+            resultsTextBox.Text += "Working . . . . . . .\r\n";
+        }
+
+        private void ResetButton_Click(object sender, EventArgs e)
+        {
+            resultsTextBox.Text = "";
+        }
+    }
+}
